f67fd7fd4643c76592c2ce03d956a40258498655
hoho108 <54274170+hoho108@users.noreply.github.com>
2019-12-17 18:01:02 +0800

    Update AIp12c.py

--- AIp12c.py
+++ AIp12c.py
@@ -90,12 +90,12 @@ def handle_message(event):
         return
     text = event.message.text
     happy = ["可愛","喜歡","愛"]
-    i = 0
     h = 0
     for i in happy:
     if text.find(i) != -1:
         if text[text.find(i)-1] != "不":
             h = 1
+            break
     if h == 1:
         reply_text = "^__^"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
