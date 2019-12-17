2e50039d648f645b249a2c0a61f280811d56c803
hoho108 <54274170+hoho108@users.noreply.github.com>
2019-12-17 20:54:14 +0800

    Update AIp12c.py

--- AIp12c.py
+++ AIp12c.py
@@ -113,45 +113,39 @@ def handle_message(event):
                 break
     if h == 1:
         reply_text = "^__^"
-        # line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
         h = 0
     elif h == -1:
         reply_text = "T__T"
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
+        for i in range(10):
+            line_bot_api.push_message(user_id, TextSendMessage(i))
+            time.sleep(1)
+        line_bot_api.push_message(user_id, TextSendMessage("冷卻完畢..."))
         h = 0
     elif (text.find("壞") != -1):
-        #line_bot_api.reply_message(event.reply_token, StickerSendMessage(package_id=11537, sticker_id=52002746))
         line_bot_api.push_message(user_id, StickerSendMessage(package_id=11537, sticker_id=52002746))
     elif (text.find("狗狗") != -1):
         reply_text = "凹嗚～"
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
-        #Your user ID
     elif(text.find("吃飯") != -1): 
         reply_text = "（踏踏踏踏...）"
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
     elif(text.find("散步") != -1):
         reply_text = "汪！"
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
     elif(text.find("握手") != -1):
         reply_text = "（伸出右手）"
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
     elif(text.find("左") != -1):
         reply_text = "（伸出左手）"
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
+
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
     elif(text.find("右") != -1):
         reply_text = "（伸出右手）"
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
     else: # 如果非以上的選項，就會學你說話
         reply_text = text
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
         line_bot_api.push_message(user_id, TextSendMessage(reply_text))
 
     try:
